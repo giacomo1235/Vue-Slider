3cc71da5120216a857575086c03e59dff1df61e3
giacomo1235 <giacomo.passariello@gmail.com>
2022-03-11 14:56:49 +0100

    fatto fino bonus 2

---
+++ js/main.js
@@ -0,0 +1,55 @@
+const app = new Vue(
+    {
+        el: '#root',
+        data:{
+            activeIndex: 0,
+            arrSlides: [
+                {
+                    title: 'Svezia',
+                    img: "img/01.jpg",
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis. '
+                },
+                {
+                    title: 'Svizzera',
+                    img: "img/02.jpg",
+                    text: 'Lorem ipsum '
+                },
+                {
+                    title: 'Gran Bretagna',
+                    img: "img/03.jpg",
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. '
+                },
+                {
+                    title: 'Germania',
+                    img: "img/04.jpg",
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, '
+                },
+                {
+                    title: 'Chieti',
+                    img: "img/05.jpg",
+                    text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, '
+                },
+            ],
+        },
+        methods: {
+            
+            next() {
+                if(this.activeIndex == this.arrSlides.length - 1) {
+                    this.activeIndex = 0;
+                } else {
+                    this.activeIndex++
+                }
+            },
+            previous() {
+                if (this.activeIndex == 0) {
+                    this.activeIndex = this.arrSlides.length - 1;
+                } else {
+                    this.activeIndex--;
+                }
+            }
+        },
+        created() {
+            setInterval(this.next, 3000)
+        }
+    }
+);
